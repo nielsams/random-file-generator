1b6d3555b336756289a87666cc5399cd1a3b2bbc
Niels Buit <buit@outlook.com>
2020-06-12 14:19:29 +0200

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nielsams/random-file-generator
+
+go 1.14
+
+require (
+	github.com/cheggaaa/pb/v3 v3.0.4
+	github.com/google/uuid v1.1.1
+)
---
+++ randomfiles.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	cryptorand "crypto/rand"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/cheggaaa/pb/v3"
+	"github.com/google/uuid"
+)
+
+func main() {
+	currentDir, _ := os.Getwd()
+	filecount := flag.Int("Count", 0, "[Required] Number of files to generate")
+	targetdir := flag.String("Dir", currentDir, "Target directory for files. Will be created if not exists.")
+	filesize := flag.String("Size", "32", "Filesizes in kilobyte. Single value or comma seperated for random distribution.")
+	flag.Parse()
+
+	if flag.NFlag() == 0 || *filecount == 0 {
+		fmt.Println("\nYou must specify at least the number of files with the -Count flag.\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	filesizes := strings.Split(*filesize, ",")
+	fmt.Printf("\nGenerating %d files of size %s in directory %s.\n", *filecount, *filesize, *targetdir)
+
+	createDirIfNotExists(*targetdir)
+	GenerateFiles(*filecount, *targetdir, filesizes)
+}
+
+// GenerateFiles is used to generate files with random content
+// count - the number of files
+// targetdir - the directory the files will be stored in
+// filesizes - a single number or comma separated list of numbers
+func GenerateFiles(count int, targetdir string, filesizes []string) {
+	bar := pb.StartNew(count)
+	for i := 0; i < count; i++ {
+		filename := fmt.Sprintf("%s\\%s.bin", targetdir, uuid.New())
+		randomfilesize, err := strconv.Atoi(filesizes[rand.Intn(len(filesizes))])
+		if err != nil {
+			fmt.Printf("Cannot parse filesizes parameter: %q", err)
+			os.Exit(1)
+		}
+		createRandomFile(filename, randomfilesize*1024)
+		bar.Increment()
+	}
+	bar.Finish()
+}
+
+func createRandomFile(name string, length int) {
+	data := make([]byte, length)
+	_, err := cryptorand.Read(data)
+
+	file, err := os.Create(name)
+	defer file.Close()
+	if err != nil {
+		fmt.Printf("Cannot create file: %q", err)
+		os.Exit(1)
+	}
+	file.Write(data)
+}
+
+func createDirIfNotExists(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			fmt.Printf("Unable to create directory for files: %q", err)
+			os.Exit(1)
+		}
+	}
+}
